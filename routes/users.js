4350bdffb42ca5a4e03eb0b96a53723803c465b0
aditya2337 <aditya.n@gai.co.in>
2017-03-06 21:08:01 +0530

    adding origin

--- routes/users.js
+++ routes/users.js
@@ -110,6 +110,7 @@ router
 // login and registration actions
 .get('/home', (req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'http://apk-decompiler.herokuapp.com');
+  res.header('Access-Control-Allow-Origin', 'http://apk-decompiler.surge.sh');
   res.header('Access-Control-Allow-Credentials', true);
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.json({
